c6f6391e86f2dd474945a01449a2d51d478bf03b
ahmed ali <ahmed_amir30@icloud.com>
2018-07-22 14:52:52 +0200

    server.js configurations

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 
 const express = require('express');
+var path=require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8080);
+
+const port =process.env.PORT || '5000';
+
+app.use(express.static(__dirname + '/public'));
+app.get('/[^\.]+$',function (req, res) {
+  res.set('Content-Type','text/html')
+    .sendFile(path.join((__dirname, '/public/index.html')))
+});
+app.listen(app.get('port'));
