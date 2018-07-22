7c1f499cad39df769ea33d98c8f597ce2e2a457c
ahmed ali <ahmed_amir30@icloud.com>
2018-07-22 15:58:26 +0200

    try deploying with gulp

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+
+gulp.task('serveprod', function() {
+  connect.server({
+    root: [your_project_path],
+    port: process.env.PORT || 5000, // localhost:5000
+    livereload: false
+  });
+});
+/*__dirname + '/dist/YoutubeReplay'*/
